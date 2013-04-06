9ac70925ee734cff43164673e9829de214360be9
Jon Wong <j@jnwng.com>
2013-04-05 17:42:07 -0700

    Bumping GEOS version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 
-default['geos']['version'] = '3.3.5'
-default['geos']['checksum'] = '2ba61afb7fe2c5ddf642d82d7b16e75b'
+default['geos']['version'] = '3.3.8'
+default['geos']['checksum'] = '1743e09f37eb75d85283a684a5765c4f44d035fa'
 default['geos']['name'] = "geos-#{default['geos']['version']}"
 default['geos']['url'] = 
   "http://download.osgeo.org/geos/#{default['geos']['name']}.tar.bz2"
